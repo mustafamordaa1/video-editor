eadf50387828d305d411564d289485620143c3b0
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2024-05-23 15:39:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,137 @@
+from telegram import *
+from telegram.ext import *
+import os
+import json
+from methods import edit_video, YTdownload, cut_video
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Welcome Sir! Just send me a video and You'll see")
+
+async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg_id = update.message.id
+    user_id = update.message.from_user.id
+    video_id = update.message.video.file_id
+    file = await context.bot.get_file(video_id)
+    await file.download_to_drive(custom_path=f'{user_id}.mp4')
+
+    try:
+        with open('users_data.json', 'r', encoding='utf-8') as outfile:
+            data = json.load(outfile)
+
+        index = 0
+        for i in data:
+            if i["id"] == user_id:
+                data.pop(index)
+                with open('users_data.json', 'w', encoding='utf-8') as outfile:
+                    json_data = json.dump(data, outfile, indent=4)
+            index +=1
+
+    except FileNotFoundError:
+        data = []
+
+    data.append({"id": user_id, "cutSilence":0, "19:6": 0, "speedup": 0, "music": 0 })
+    with open('users_data.json', 'w', encoding='utf-8') as outfile:
+        json_data = json.dump(data, outfile, indent=4)
+
+    buttons = [[InlineKeyboardButton('إزالة الفراغات', callback_data=f'{msg_id+1}&cutSilence')], [InlineKeyboardButton('تحويل المقطع طولياً', callback_data=f'{msg_id+1}&19:6')],
+                [InlineKeyboardButton('تسريع المقطع ل58 ثانية', callback_data=f'{msg_id+1}&speedup')], [InlineKeyboardButton('اضافة موسيقا تصويرية', callback_data=f'{msg_id+1}&music')], [InlineKeyboardButton('تأكيد', callback_data=f'{msg_id+1}&done')]]
+    await update.message.reply_text("اختر الخيارات التي تريد تطبيقها ثم اضغط على تأكيد", reply_markup = InlineKeyboardMarkup(buttons))
+    
+async def handle_url(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = user_id = update.message.from_user.id
+    link = user_id = update.message.text
+    file = YTdownload(user_id, link)
+
+    if file:
+        #file = cut_video(id, file, 20, 50)
+        file = edit_video(file, 1, 0, 0, 0)
+        await update.message.reply_text(text="Done!")
+        await update.message.reply_document(document=file)
+        #os.remove(f'{file}')
+    
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id =  update.callback_query.message.chat.id
+    query = update.callback_query.data.split('&')
+    msg_id, request_type = int(query[0]), query[1]
+
+    try:
+        index = 0
+
+        with open('users_data.json', 'r', encoding='utf-8') as outfile:
+            data = json.load(outfile)
+        
+        for i in data:
+            if i["id"] == user_id:
+                break
+
+            index += 1
+            
+    except FileNotFoundError:
+        data = []
+
+        return 0
+
+    if request_type == 'cutSilence':
+        data[index]["cutSilence"] = 1
+    elif request_type == '19:6':
+        data[index]["19:6"] = 1
+    elif request_type == 'speedup':
+        data[index]["speedup"] = 1
+    elif request_type == 'music':
+        data[index]["music"] = 1
+
+    with open('users_data.json', 'w', encoding='utf-8') as outfile:
+        json_data = json.dump(data, outfile, indent=4)
+
+
+    added = []
+    buttons = [[InlineKeyboardButton('إزالة الفراغات', callback_data=f'{msg_id}&cutSilence')], [InlineKeyboardButton('تحويل المقطع طولياً', callback_data=f'{msg_id}&19:6')],
+                [InlineKeyboardButton('تسريع المقطع ل58 ثانية', callback_data=f'{msg_id}&speedup')], [InlineKeyboardButton('اضافة موسيقا تصويرية', callback_data=f'{msg_id}&music')], [InlineKeyboardButton('تأكيد', callback_data=f'{msg_id}&done')]]
+    if data[index]["cutSilence"]:
+        added.append('-إزالة الفراغات')
+        buttons.remove([InlineKeyboardButton('إزالة الفراغات', callback_data=f'{msg_id}&cutSilence')])
+    if data[index]["19:6"]:
+        added.append('-تحويل المقطع طولياً')
+        buttons.remove([InlineKeyboardButton('تحويل المقطع طولياً', callback_data=f'{msg_id}&19:6')])
+    if data[index]["speedup"]:
+        added.append('-تسريع المقطع ل58 ثانية')
+        buttons.remove([InlineKeyboardButton('تسريع المقطع ل58 ثانية', callback_data=f'{msg_id}&speedup')])
+    if data[index]["music"]:
+        added.append('-اضافة موسيقا تصويرية')
+        buttons.remove([InlineKeyboardButton('اضافة موسيقا تصويرية', callback_data=f'{msg_id}&music')])
+
+    await context.bot.edit_message_text(chat_id=user_id, message_id=msg_id, text=f"الخيارات التي اخترتها:({' - '.join(added)}) اضغط على تأكيد عندما تنتهي من الاختيار..")
+    await context.bot.edit_message_reply_markup(chat_id=user_id, message_id=msg_id, reply_markup = InlineKeyboardMarkup(buttons))
+    
+    if request_type == 'done':
+        await context.bot.delete_message(update.callback_query.message.chat.id, update.callback_query.message.id)
+        await context.bot.send_message(user_id, text='تجري معالجة طلبك..')
+        print(data[index])
+        file = edit_video(user_id, data[index]["cutSilence"], data[index]["19:6"], data[index]["speedup"], data[index]["music"])
+
+
+        data.pop(index)
+        with open('users_data.json', 'w', encoding='utf-8') as outfile:
+            json_data = json.dump(data, outfile, indent=4)
+
+        await context.bot.send_message(user_id, text="Done!")
+        await context.bot.send_document(user_id, document=f'{file}')
+        os.remove(f'{file}')
+
+
+
+
+app = ApplicationBuilder().token("6572714563:AAFDUiD1z-U88NbI1GMffs1jO9HwCCs0-Ps").build()
+
+app.add_handler(CommandHandler("start", hello))
+
+app.add_handler(MessageHandler(filters.VIDEO, handle_video))
+app.add_handler(MessageHandler(filters.TEXT, handle_url))
+app.add_handler(CallbackQueryHandler(button))
+
+app.run_polling()
+
+#pip install Pillow==9.5.0
---
+++ methods.py
@@ -0,0 +1,96 @@
+import moviepy.editor as mpy
+from moviepy.video.fx.all import crop
+import os
+from pytube import YouTube 
+
+def YTdownload(id, link):
+
+    SAVE_PATH = "."
+
+    try: 
+        yt = YouTube(link) 
+    except: 
+
+        return False
+
+    mp4_streams = yt.streams.filter(file_extension='mp4').all()
+
+    d_video = yt.streams.get_by_itag(22)
+
+    try: 
+        d_video.download(output_path=SAVE_PATH)
+        print('Video downloaded successfully!')
+
+        return d_video.default_filename
+
+
+
+    except: 
+        print("Some Error!")
+   
+
+def cut_video(id, file, start, end):
+
+    clip =  mpy.VideoFileClip(file) 
+    clip = clip.subclip(start, end) 
+    clip.write_videofile(f'{id}-done.mp4')
+
+    os.remove(file)
+    return f'{id}-done.mp4'
+
+def edit_video(id, cutSilence, ratio, speedup, music):
+
+    filename = id
+
+    if cutSilence:
+        os.system(f"auto-editor {filename}.mp4 --edit audio:threshold=-19dB")
+
+        filename = f"{filename}_ALTERED.mp4"
+
+    if speedup:
+        clip = mpy.VideoFileClip(f'{filename}.mp4')
+        duration = clip.duration 
+        os.system(f"auto-editor {filename}.mp4 --video-speed {(duration/60)} --silent-speed 99999")
+
+        filename = f"{filename}_ALTERED.mp4"
+        
+    if ratio:
+        clip = mpy.VideoFileClip(f'{filename}.mp4')
+        (w, h) = clip.size
+
+        crop_width = h * 9/16
+        
+        x1, x2 = (w - crop_width)//2, (w+crop_width)//2
+        y1, y2 = 0, h
+        cropped_clip = crop(clip, x1=x1, y1=y1, x2=x2, y2=y2)
+
+        if not music:
+            cropped_clip.write_videofile(f'{id}-done.mp4')
+            filename = f'{id}-done'
+
+        
+    if music:    
+        if not ratio:
+            cropped_clip = mpy.VideoFileClip(f'{filename}.mp4')
+
+        audio_background = mpy.AudioFileClip('effect_1.mp3')
+        final_audio = mpy.CompositeAudioClip([cropped_clip.audio, audio_background])
+        final_clip = cropped_clip.set_audio(final_audio)
+        final_clip.write_videofile(f'{id}-done.mp4')
+
+        filename = f'{id}-done'
+    
+    '''
+    
+    if cutSilence and speedup:
+        os.remove(f'{id}_ALTERED_ALTERED.mp4')
+        os.remove(f'{id}_ALTERED.mp4')
+    elif cutSilence or speedup:
+        os.remove(f'{id}_ALTERED.mp4')
+        
+    os.remove(f'{id}.mp4')
+'''
+    return f'{filename}.mp4'
+    
+
+
